4f1de51b142e6db2571326eb3cebc7a204762e32
youri47 <youri93400@gmail.com>
2026-01-19 01:45:26 +0000

    add py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
-
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    
     names = dir(hidden_4)
-    
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
