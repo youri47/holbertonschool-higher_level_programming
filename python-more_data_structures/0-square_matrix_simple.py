a08ac115ff782ff51df1e58a2f7e4fb4334da53e
youri47 <youri93400@gmail.com>
2026-01-22 12:19:51 +0000

    add map

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
+#!/usr/bin/python3
+"""Module qui contient la fonction square_matrix_simple"""
+
+
 def square_matrix_simple(matrix=[]):
+    """Retourne une nouvelle matrice avec les valeurs au carré"""
     return [[x ** 2 for x in row] for row in matrix]
