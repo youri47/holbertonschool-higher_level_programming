8263788211a955b525788bee7dcbc1bddebd97f0
youri47 <youri93400@gmail.com>
2026-01-22 12:57:27 +0000

    add number key

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module qui contient la fonction number_keys"""
+
+
+def number_keys(a_dictionary):
+    """Retourne le nombre de clés dans un dictionnaire"""
+    return len(a_dictionary)
