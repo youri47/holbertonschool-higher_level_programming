135f00244acff3d7724bd5e69568b7eb8db33ab7
youri47 <youri93400@gmail.com>
2026-01-22 12:40:37 +0000

    uniq add

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module qui contient la fonction uniq_add"""
+
+
+def uniq_add(my_list=[]):
+    """Additionne tous les entiers uniques d'une liste"""
+    return sum(set(my_list))
