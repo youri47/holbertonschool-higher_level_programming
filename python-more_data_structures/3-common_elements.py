d98f335d7b17fb14914337eea6f5d6b3cb60ffb3
youri47 <youri93400@gmail.com>
2026-01-22 12:47:02 +0000

    add comment element

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module qui contient la fonction common_elements"""
+
+
+def common_elements(set_1, set_2):
+    """Retourne un set des éléments communs aux deux sets"""
+    return set_1 & set_2
