58837c40a5fc4da5dc13da1e875dedf8e198fc0e
youri47 <youri93400@gmail.com>
2026-01-23 10:38:01 +0000

    add

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def raise_exception():
+    """Lève une exception TypeError."""
+    raise TypeError
---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Lève une NameError avec un message."""
+    raise NameError(message)
