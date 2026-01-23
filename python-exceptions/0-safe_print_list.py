ae537131fdc8cad387f439d8344e0378d8a5c74f
youri47 <youri93400@gmail.com>
2026-01-23 10:22:18 +0000

    add list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Imprime x éléments d'une liste de manière sécurisée."""
+    count = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+
+    print()  # Nouvelle ligne à la fin
+    return count
