d90ec65102a9898178011308003c4be371e559ed
youri47 <youri93400@gmail.com>
2026-01-28 23:41:43 +0000

    add sqare

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is class Square that defines a square by size.
+"""
+
+
+class Square:
+    """
+    A class that defines a square by its size.
+    """
+    def __init__(self, size):
+        self.__size = size
---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is class Square that defines a square by size.
+"""
+
+
+class Square:
+    """
+    A class that defines a square by its size.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
