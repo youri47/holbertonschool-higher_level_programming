fd4fd2efc47af22cd13821459e1a1e560ba368f0
youri47 <youri93400@gmail.com>
2026-02-06 15:26:43 +0000

    add abc

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
+
 from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
-    """Classe abstraite - on ne peut PAS l'instancier directement."""
 
     @abstractmethod
     def sound(self):
-        """Chaque animal DOIT définir son propre son."""
         pass
 
 
 class Dog(Animal):
-    """Hérite de Animal, donc DOIT implémenter sound()."""
 
     def sound(self):
         return "Bark"
 
 
 class Cat(Animal):
-    """Hérite de Animal, donc DOIT implémenter sound()."""
 
     def sound(self):
         return "Meow"
