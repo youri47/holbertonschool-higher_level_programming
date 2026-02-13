c98228ee4fd2749242a7f14e240d03354030e68f
youri47 <youri93400@gmail.com>
2026-02-13 22:22:33 +0000

    add student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Defines a Student class with JSON serialization and optional filtering.
-"""
+"""Writing a class Student"""
+
 
 class Student:
-    """Represents a student with first name, last name, and age."""
-
+    """Instantiation """
     def __init__(self, first_name, last_name, age):
-        """Initialize a student with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.age = age 
 
     def to_json(self, attrs=None):
-        """
-        Return a dictionary representation of the student.
-
-        If attrs is a list of strings, only include those attributes
-        that exist in the instance. Otherwise, include all attributes.
-        """
         if attrs is None:
-            return self.__dict__.copy()  # Return all attributes
-        return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+            return self.__dict__
+        else:
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
