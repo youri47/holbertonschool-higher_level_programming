1b934f19cf11f4620b9f92c95a05401335224b01
youri47 <youri93400@gmail.com>
2026-02-13 22:25:51 +0000

    add student

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-""" Student module with JSON serialization and reloading """
+"""Writing a class Student"""
+
 
 class Student:
-    """Defines a student with first_name, last_name, and age."""
-
+    """Instantiation """
     def __init__(self, first_name, last_name, age):
-        """Initialize a student."""
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.age = age 
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of the student.
-
-        If attrs is a list of strings, only return those attributes.
-        Otherwise, return all attributes.
-        """
         if attrs is None:
-            return self.__dict__.copy()
-        filtered = {}
-        for key in attrs:
-            if key in self.__dict__:
-                filtered[key] = self.__dict__[key]
-        return filtered
+            return self.__dict__
+        else:
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
 
     def reload_from_json(self, json):
-        """Replace all attributes of the student with the dictionary values."""
+        """Replace attributes of Student from a dict"""
         for key, value in json.items():
             setattr(self, key, value)
