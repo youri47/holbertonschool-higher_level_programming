635fa0e52ae217823715affa5fabcfb8a11a4dbd
youri47 <youri93400@gmail.com>
2026-02-13 22:13:16 +0000

    add append write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Module qui ajoute du texte à la fin d'un fichier"""
+
+"""Append a string at the end of a txt file return the nb of char."""
 
 
 def append_write(filename="", text=""):
-    """Ajoute une string à la fin d'un fichier UTF-8 et retourne le nombre de caractères ajoutés"""
-    with open(filename, mode="a", encoding="utf-8") as f:
-        return f.write(text)
+    """append_write"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        content = f.write(text)
+        return content
