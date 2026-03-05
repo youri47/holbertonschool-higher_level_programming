8f9cbb27dbbf7aaeec431bb2efc53d1b54021f90
Your Name <you@example.com>
2026-03-05 14:04:49 +0100

    add score

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Lists records with score >= 10 ordered by score
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- Computes the average score
+SELECT AVG(`score`) AS `average` FROM `second_table`;
