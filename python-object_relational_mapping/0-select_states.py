1081d62f582ce47aa53358bb5227bbba2f7a49b3
Your Name <you@example.com>
2026-03-06 20:17:48 +0100

    add select state

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
 """
-0-select_states.py
-
-Lists all states from the database hbtn_0e_0_usa
+Module that lists all states from the database hbtn_0e_0_usa.
 """
+
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    """
-    Main function:
-    that executes the database
-    connection and retrieval of states.
-    """
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     db = MySQLdb.connect(
         host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
