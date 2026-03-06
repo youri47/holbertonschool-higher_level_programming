4b8f0679e45ab73e56a10040cf26e4dcdb8a0dee
youri47 <youri93400@gmail.com>
2026-03-06 22:45:56 +0000

    task

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,4 +1,3 @@
-cat > 5-filter_cities.py << 'EOF'
 #!/usr/bin/python3
 """Lists all cities of a given state from hbtn_0e_4_usa."""
 
@@ -15,12 +14,12 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT cities.name \
-FROM cities JOIN states ON cities.state_id = states.id \
-WHERE states.name = %s \
-ORDER BY cities.id ASC", (sys.argv[4],))
+    cur.execute("SELECT cities.name"
+                " FROM cities JOIN states"
+                " ON cities.state_id = states.id"
+                " WHERE states.name=%s"
+                " ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
-EOF
