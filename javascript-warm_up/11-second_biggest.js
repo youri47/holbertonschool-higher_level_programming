c86be7ec9eeb9d6399a0ca8ba12fe47bb3c7337a
youri47 <youri93400@gmail.com>
2026-03-20 00:04:07 +0000

    add java

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ const args = process.argv.slice(2).map(Number);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  args.sort((a, b) => b - a);
-  console.log(args[1]);
+  const sorted = args.sort((a, b) => b - a);
+  console.log(sorted[1]);
 }
