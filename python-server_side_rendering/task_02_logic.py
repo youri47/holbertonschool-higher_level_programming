0ac4302fe22c740e75df7907f24580de3a3d6be8
youri47 <youri93400@gmail.com>
2026-03-26 23:28:51 +0000

    add task 02

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+    items_list = data.get("items", [])
+    return render_template('items.html', items=items_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
